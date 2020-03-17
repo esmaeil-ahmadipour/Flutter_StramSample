132792bea96e8c51f03f067fbd11a207087ad5f0
Esmaeil Ahmadipour <software8686@gmail.com>
2020-03-17 23:42:06 +0330

    Initial commit

---
+++ android/app/src/main/kotlin/ir/ea2/flutterrxsample/MainActivity.kt
@@ -0,0 +1,6 @@
+package ir.ea2.flutterrxsample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutterrxsample/home_page.dart';
+
+void main() {
+  runApp(HomePage());
+}
